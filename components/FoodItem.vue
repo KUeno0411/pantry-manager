79e9f9772afe65760e9dc01681ab5218bfc4a0da
toki23 <blancshio@gmail.com>
2021-08-23 23:21:21 +0900

    メインページをちょっと書いた

---
+++ components/FoodItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex">
+    <img class="mr-2"src="/favicon.ico" width="60" height="60"/>
+    <div>
+      <p>{{ name }}</p>
+      <p>賞味期限までおよそあと{{ limit }}日</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FoodItem",
+  props: ['name', 'limit']
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <Tutorial/>
+  <div>
+    <ul>
+      <li class="border-2 " v-for="item in this.foods" key="item.id">
+        <FoodItem :name="item.name" :limit="item.limit"></FoodItem>
+      </li>
+    </ul>
+    <button class=" border-blue-400 bg-blue-300 border-2 hover:bg-blue-700 fixed right-5 bottom-5 shadow-xl">レシートのスキャン
+    </button>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      foods: [
+        {name: "卵", limit: "3", id: 1},
+        {name: "牛肉", limit: "8", id: 2},
+        {name: "玉ねぎ", limit: "9", id: 3},
+      ]
+    }
+  }
+}
 </script>
